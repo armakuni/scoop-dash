d8b77beeaa139de7c72869620e394b8250d14566
Ashish Sehra <ashish.sehra@armakuni.com>
2024-06-06 14:24:11 +0530

    Add eslint:recommended package and fix lint errors
    Co-authored-by: Billie Thompson <billie.thompson@armakuni.com>

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,19 +1,18 @@
-import {describe, it, expect} from "vitest";
+import { describe, it, expect } from "vitest";
 
-import {screen, render} from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import Home from "./page";
 
-describe('Coming soon page', () => {
-    it('should have the title of the website', async () => {
-        const page = render(<Home />);
+describe("Coming soon page", () => {
+  it("should have the title of the website", async () => {
+    render(<Home />);
 
-        expect(await screen.findByText("ScoopDash")).toBeVisible();
-    });
+    expect(screen.getByRole("heading", { name: "ScoopDash" })).toBeVisible();
+  });
 
-    it('should say that the app is coming soon', async () => {
-        const page = render(<Home />);
+  it("should say that the app is coming soon", async () => {
+    render(<Home />);
 
-        expect(await screen.findByText("Coming Soon!")).toBeVisible();
-    });
-
+    expect(screen.getByRole("heading", { name: "Coming Soon!" })).toBeVisible();
+  });
 });
--- src/util.test.ts
+++
@@ -1,7 +0,0 @@
-import {describe, expect, it} from "vitest";
-
-describe('Utils', () => {
-    it('should work', () => {
-        expect(true).toBeTruthy();
-    });
-});
