e68a220afcdbdedac65edea31343878b5085d3a0
Billie Thompson <billie.thompson@armakuni.com>
2024-06-10 11:24:01 +0200

    Mock the font call
    Co-authored-by: Mark Bradley <mark.bradley@armkauni.com>

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,11 @@
-import '@testing-library/jest-dom/vitest'
+import "@testing-library/jest-dom/vitest";
+import { vi } from "vitest";
+import { Lobster_Two } from "next/font/google";
+
+vi.mock("next/font/google", () => ({
+  Lobster_Two: () => ({
+    style: {
+      fontFamily: "mocked",
+    },
+  }),
+}));
